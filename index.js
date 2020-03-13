274d49ea9d1c1d24dbe1e1752ad3325f14718f64
David Dumolo <ddumolo@hotmail.com>
2020-03-13 18:04:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   } else {
 
-    let total = total();
+    let total =total();
     return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`
   }
 
