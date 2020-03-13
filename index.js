bd3841ae02c6999b4461bdc5c57d96cb360380e5
David Dumolo <ddumolo@hotmail.com>
2020-03-13 11:53:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function viewCart() {
     } else if(cart.length == 2) {
       string = string + `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
     } else {
-      string = string + `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}, and ${cart[2].itemName} at $${cart[2].itemPrice}.`
+      string = string + `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, ${cart[1].itemName} at $${cart[1].itemPrice}, and ${cart[2].itemName} at $${cart[2].itemPrice}.`
     }
     return string
   }
