92027f37892600db23fa037268fac107da5268bd
David Dumolo <ddumolo@hotmail.com>
2020-03-13 18:00:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -69,8 +69,8 @@ function placeOrder(cardNumber) {
   if(!cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   } else {
-    total = total()
-    cart = []
+    total = total();
+    cart = [];
     return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`
   }
 
