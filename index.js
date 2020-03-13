a140a48746f4acc7dc9b6a9135920a97368f187b
David Dumolo <ddumolo@hotmail.com>
2020-03-13 11:32:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,10 +12,9 @@ function setCart(c) {
 function addToCart(item) {
  // write your code here
  var cart = []
- var price =
- cart.push({itemName: item, itemPrice: Math.floor(Math.random() * 100) + 1})
- var temp = cart.slice(-1)[0]
- return temp.itemName + " has been added to your cart"
+ var object = {}
+ object.itemName = item
+ object.itemPrice = Math.floor(Math.random() * 100) + 1
 
 }
 
