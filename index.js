54392bd28ca561f8b5a8255126b7f8d0f0ba2ab7
David Dumolo <ddumolo@hotmail.com>
2020-03-13 17:41:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ function total() {
 
 function removeFromCart(item) {
   // write your code here
-  
+
   for(let i = 0; i < cart.length; i++) {
     if (cart[i].itemName == item) {
       cart = cart.splice(i, 1)
@@ -56,7 +56,7 @@ function removeFromCart(item) {
     } else {
       return "That item is not in your cart."
     }
-  }  
+  }
 }
 
 function placeOrder(cardNumber) {
